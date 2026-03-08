f311b5d17808d7d49e6f5a49428feba321f2ec60
Ihor Haidai <igayday74@gmail.com>
2026-03-07 19:09:57 -0500

    Solve Cypress e2e task

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('https://conduit.mate.academy/user/login');
+  });
 
+  it('should provide an ability to log in', () => {
+    const email = 'test1234567@gmail.com';
+    const pw = 'test1234567';
+
+    cy.get('h1').should('have.text', 'Sign in');
+
+    cy.get('input[placeholder="Email"]').type(email);
+    cy.get('input[placeholder="Password"]').type(pw);
+
+    cy.get('button.btn').click();
+
+    cy.contains('a.nav-link', 'test1234567')
+      .should('be.visible');
   });
 });
